6b820652d61e031483d38fc037aec7d9e8340af3
Bulatik <kmpchk@gmail.com>
2019-07-27 13:28:33 +0300

    + PROGRAM_DATA structure

---
+++ programdata.h
@@ -0,0 +1,21 @@
+#ifndef PROGRAMDATA_H
+#define PROGRAMDATA_H
+
+#include <stdint.h>
+#include <QString>
+
+typedef struct _PROGRAM_DATA
+{
+    QString inputImageFilename;
+    QString outputModelFilename;
+    QString outputFolderPath = ".";
+    uint8_t convertMode;
+    bool bColorInverseOption = false;
+    bool bGreyscaleOption = false;
+    double dZetAxisCoeff = 0.25;
+    double dDimensionCoeff = 0.05;
+    double dXAxisRotateAngle = -90.0;
+
+} PROGRAM_DATA, *PPROGRAM_DATA;
+
+#endif // PROGRAMDATA_H
