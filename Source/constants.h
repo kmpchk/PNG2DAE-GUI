f4240be819fd6e2f7379425a7e4e816ee4986db4
Bulatik <kmpchk@gmail.com>
2019-07-30 13:45:02 +0300

    + application constanst

---
+++ Source/constants.h
@@ -0,0 +1,19 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <QString>
+
+static const int WINDOW_WIDTH = 279;
+static const int WINDOW_HEIGHT = 448;
+static const int RESIZED_WINDOW_WIDTH = 555;
+static const double ZET_SCALE_COEFF_BOTTOM = 0.0;
+static const double ZET_SCALE_COEFF_TOP = 50.0;
+static const double DIMENSION_SCALE_COEFF_BOTTOM = 0.0;
+static const double DIMENSION_SCALE_COEFF_TOP = 50.0;
+static const double X_ROTATE_ANGLE_BOTTOM = 0.0;
+static const double X_ROTATE_ANGLE_TOP = 360.0;
+static const int INPUT_IMAGE_VIEW_WIDTH = 271;
+static const int INPUT_IMAGE_VIEW_HEIGHT = 231;
+static const QString INPUT_IMAGE_PREPARED_NAME = "PNG2DAEIMAGE.png";
+
+#endif // CONSTANTS_H
